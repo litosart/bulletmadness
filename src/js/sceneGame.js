9d09a85e3fb888e139818a42a32cd284660166c2
Carlos <61838555+litosart@users.noreply.github.com>
2020-11-30 12:30:59 +0100

    movement

--- src/js/sceneGame.js
+++ src/js/sceneGame.js
@@ -5,6 +5,9 @@ class sceneGame extends Phaser.Scene {
   }
 
   preload(){
+    this.add.text(20,20,"Loading Game...", {
+      font:"25px Arial", fill:"white"
+    });
     this.load.image("background", "resources/img/backgrounds/Background_ClearSky.png");
     this.load.image("playerShip1", "resources/img/sprites/PlayerShip1.png");
   }
@@ -13,10 +16,24 @@ class sceneGame extends Phaser.Scene {
     this.background = this.add.image(0,0,"background");
     this.background.setOrigin(0,0);
 
-    this.ship = this.add.image(config.width/2,config.height/2,"playerShip1");
+    this.player1 = this.add.image(config.width/2,config.height/2,"playerShip1");
+    //(this.player1.setCollideWorldBounds(true);
 
-    this.add.text(20,20,"Loading Game...", {
-      font:"25px Arial", fill:"white"
-    });
+
+  }
+
+  update() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    if (this.cursors.left.isDown) {
+      this.player1.x -= 3;
+    } else if (this.cursors.right.isDown) {
+      this.player1.x += 3;
+    } else if (this.cursors.up.isDown) {
+      this.player1.y -= 3;
+    } else if (this.cursors.down.isDown) {
+      this.player1.y += 3;
+    }
+
   }
 }
