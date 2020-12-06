6179f7675b7c1aed722b71947d4a5e98f742f0ac
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2020-12-06 21:42:36 +0100

    T

---
+++ src/js/enemy.js
@@ -0,0 +1,25 @@
+
+class Enemy extends Phaser.GameObjects.Sprite{
+
+    constructor(scene){
+      super(scene,Phaser.Math.Between(0, config.width),0,"playerShip1");
+
+      //Adding to scene
+      scene.add.existing(this,0);
+
+      //Enabling Physics
+      scene.physics.world.enable([this]);
+
+      //Rotation in Radians
+      this.rotation = 3.1415;
+    }
+
+    Move(){
+      var speed = 150;
+      this.body.setVelocityY(speed);
+    }
+
+    create(){
+      this.Move();
+    }
+}
