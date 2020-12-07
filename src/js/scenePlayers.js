29e9fbbfb24a8ee0aba2fbd4476eccc09eb30ae0
Carlos <61838555+litosart@users.noreply.github.com>
2020-12-07 11:35:12 +0100

    Update scenePlayers.js

--- src/js/scenePlayers.js
+++ src/js/scenePlayers.js
@@ -1,7 +1,11 @@
+var  mouse = 0;
+
 class ScenePlayers extends Phaser.Scene {
   constructor(){
     super("ScenePlayers");
   }
+
+
 
   preload(){
     this.load.image("1p", "resources/img/buttons/1.png");
@@ -13,11 +17,18 @@ class ScenePlayers extends Phaser.Scene {
     this.twoplayer = this.add.image(350,500, '2p').setScale(4);
     this.oneplayer.setInteractive();
     this.twoplayer.setInteractive();
-    this.oneplayer.on('pointerdown', () =>  this.scene.start("SceneGame"));
-    this.twoplayer.on('pointerdown', () =>  this.scene.start("SceneGame"));
+    this.oneplayer.on('pointerdown', () => mouse = 1);
+    this.twoplayer.on('pointerdown', () => mouse = 2);
   }
 
   update(){
+
+    if(mouse==1){
+      this.scene.start("SceneGame")
+    } else if(mouse==2) {
+      this.scene.start("SceneGame")
+    }
+
   }
 
 }
