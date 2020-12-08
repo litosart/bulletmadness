db706846eec0d8716c154213acd90687abce10ec
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2020-12-08 18:01:28 +0100

    Separated EnemySpawner into own file
    Separated EnemySpawner into own file

--- src/js/managers/enemySpawner.js
+++ src/js/managers/enemySpawner.js
@@ -1,25 +1,30 @@
 class EnemySpawner {
 
-  constructor(scene,enemyArray) {
+  constructor(scene, enemyArray) {
     this.scene = scene;
     this.enemyList = enemyArray;
+    this.setupSpawnLoop();
+
+    this.enemyDataContainer = new EnemyShipsData();
   }
 
   spawnEnemy() {
 
     //Gets a random enemyData from the enemyDataContainer and spawns an enemy with given Data
-    var enemyDataContainer = new EnemyShipsData();
-    var enemyData = enemyDataContainer.list[Phaser.Math.Between(0, enemyDataContainer.list.length - 1)]
+    var enemyData = this.enemyDataContainer.list[Phaser.Math.Between(0, this.enemyDataContainer.list.length - 1)]
+
+    //Creates enemy
+    var enemy = new Enemy(this.scene, enemyData);
 
     //Adds enemy to enemyList
-    this.enemyList.push(new Enemy(this.scene, enemyData));
+    this.enemyList.push(enemy);
 
   }
 
   setupSpawnLoop() {
 
     var timerEventConfig = {
-      delay: this.timeBetweenSpawns,
+      delay: 100,
       loop: true,
       callback: this.spawnEnemy,
       callbackScope: this
