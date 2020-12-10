c2d1170d9bb655b601ab7a4166c302cf333b79a0
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2020-12-10 12:07:55 +0100

    Setup Spawners
    Setup Spawners

--- src/js/scenes/sceneGame.js
+++ src/js/scenes/sceneGame.js
@@ -5,7 +5,7 @@ class SceneGame extends Phaser.Scene {
   }
 
   create(sceneData) {
-
+    
     //Setup previous Scene Data
     this.sceneData = sceneData;
 
@@ -23,9 +23,18 @@ class SceneGame extends Phaser.Scene {
 
     //Enemy Manager Initialization
     this.enemyManager = new EnemyManager(this);
-    this.enemyManager.setupSpawner(enemyShipsData.list[0],{spawnDelay: 1500,loop: true});
-    this.enemyManager.setupSpawner(enemyShipsData.list[2],{spawnDelay: 500,loop: true});
-    this.enemyManager.setupSpawner(enemyShipsData.list[3],{spawnDelay: 200,loop: true});
+    this.enemyManager.setupSpawner(enemyShipsData.list[0], {
+      spawnDelay: 1500,
+      loop: true
+    });
+    this.enemyManager.setupSpawner(enemyShipsData.list[2], {
+      spawnDelay: 500,
+      loop: true
+    });
+    this.enemyManager.setupSpawner(enemyShipsData.list[3], {
+      spawnDelay: 200,
+      loop: true
+    });
 
     //Score Manager Initialization
     this.scoreManager = new ScoreManager(this, this.playerManager.playersArray);
