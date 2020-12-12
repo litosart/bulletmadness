96890632006789eb8a1a921301a82589e29dd314
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2020-12-12 01:01:44 +0100

    Fixed Powerup Image
    Fixed Powerup Image

--- src/js/entities/powerup.js
+++ src/js/entities/powerup.js
@@ -4,7 +4,8 @@ class PowerUp extends Phaser.GameObjects.Sprite {
   powerupData;
 
   constructor(scene, powerupData) {
-    super(scene, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height), "ship_player_1");
+    super(scene, Phaser.Math.Between(0, config.width), Phaser.Math.Between(0, config.height), "powerup_movement_speed_boost");
+    this.setScale(1.5);
     this.scene = scene;
     this.powerupData = powerupData;
 
