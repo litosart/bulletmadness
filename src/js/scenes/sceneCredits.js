56ca1918c88fd6c3d7d111428125ecbd58ba7d93
Anthonys-source <62327779+Anthonys-source@users.noreply.github.com>
2021-01-18 18:58:42 +0100

    Update sceneCredits.js

--- src/js/scenes/sceneCredits.js
+++ src/js/scenes/sceneCredits.js
@@ -9,11 +9,17 @@ class SceneCredits extends Phaser.Scene {
     this.gobackb.setInteractive();
     this.gobackb.on('pointerdown', () => this.scene.start("SceneTitleScreen"));
 
-    this.creditsText = this.add.bitmapText(80, 120, "font_default", "Carlos Rodriguez Sanchez-Roldan").setFontSize(40);
-    this.creditsText = this.add.bitmapText(80, 170, "font_default", "Jose Fernando Medina Talavera").setFontSize(40);
-    this.creditsText = this.add.bitmapText(80, 220, "font_default", "Antonio Arian Silaghi").setFontSize(40);
-    this.creditsText = this.add.bitmapText(80, 270, "font_default", "Norman Peter Stankewitz").setFontSize(40);
-    this.creditsText = this.add.bitmapText(80, 320, "font_default", "Music by: David Aguilar Guerra").setFontSize(40);
+    // this.creditsText = this.add.bitmapText(80, 120, "font_default", "Carlos Rodríguez Sanchez-Roldán").setFontSize(40);
+    // this.creditsText = this.add.bitmapText(80, 170, "font_default", "José Fernando Medina Talavera").setFontSize(40);
+    // this.creditsText = this.add.bitmapText(80, 220, "font_default", "Antonio Arian Silaghi").setFontSize(40);
+    // this.creditsText = this.add.bitmapText(80, 270, "font_default", "Norman Peter Stankewitz").setFontSize(40);
+    // this.creditsText = this.add.bitmapText(80, 320, "font_default", "Music by: David Aguilar Guerra").setFontSize(40);
+
+    this.creditsText = this.add.text(80, 120, 'Carlos Rodríguez Sanchez-Roldán', {color:"#C70039"}).setFontSize(25);
+    this.creditsText = this.add.text(80, 170, 'José Fernando Medina Talavera', {color:"#C70039"}).setFontSize(25);
+    this.creditsText = this.add.text(80, 220, 'Antonio Arian Silaghi', {color:"#C70039"}).setFontSize(25);
+    this.creditsText = this.add.text(80, 270, 'Norman Peter Stankewitz', {color:"#C70039"}).setFontSize(25);
+    this.creditsText = this.add.text(80, 320, 'Music by: David Aguilar Guerra', {color:"#C70039"}).setFontSize(25);
 
     //Scene Fade In
     this.cameras.main.fadeIn(500);
